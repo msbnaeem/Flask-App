b0117d5e9c8cb1de732bff0d93fda9d98a8dd1f3
Mohammad Naeem <binnaeem96@gmail.com>
2021-11-18 10:47:51 -0500

    added registration functionality

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -9,8 +9,11 @@ from flask import redirect, url_for
 from database import db
 from models import Note
 from models import User
-
+from forms import RegisterForm
+from flask import session
+import bcrypt
 app = Flask(__name__)  # create an app
+
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -27,11 +30,14 @@ def index():
 @app.route('/notes')
 def get_notes():
     # retrieve user from database
-    a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
-    # retrieve notes from database
-    my_notes = db.session.query(Note).all()
+    # a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
+    if session.get('user'):
+        # retrieve notes from database
+        my_notes = db.session.query(Note).all()
 
-    return render_template('notes.html', notes=my_notes, user=a_user)
+        return render_template('notes.html', notes=my_notes, user=session['user'])
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/notes/<note_id>')
@@ -71,6 +77,7 @@ def update_note(note_id):
 
         return render_template('new.html', note=my_note, user=a_user)
 
+
 @app.route('/notes/delete/<note_id>', methods=['POST'])
 def delete_note(note_id):
     # retrieve note from database
@@ -78,10 +85,33 @@ def delete_note(note_id):
     db.session.delete(my_note)
     db.session.commit()
 
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # salt and hash password
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        # get entered user data
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        # create user model
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+        # add user to database and commit
+        db.session.add(new_user)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        session['user_id'] = new_user.id  # access id value from user model of this newly added user
+        # show user dashboard view
+        return redirect(url_for('get_notes'))
+
+    # something went wrong - display register view
+    return render_template('register.html', form=form)
 
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
-
     # a_user = {'name': 'Mohammad', 'email': 'mogli@uncc.edu'}
     a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
     # check method used for request
@@ -113,14 +143,16 @@ def new_note():
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SECRET_KEY'] = 'SE3155'
 
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 
 # Setup models
 with app.app_context():
-    db.create_all()   # run under the app context
+    db.create_all()  # run under the app context
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)), debug=True)
 
