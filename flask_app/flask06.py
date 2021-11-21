54f02d168f081033b0289f405a5f83822c95fd5f
Mohammad Naeem <binnaeem96@gmail.com>
2021-11-21 13:13:30 -0500

    updated user specific routed to check for session data

--- flask_app/flask06.py
+++ flask_app/flask06.py
@@ -1,5 +1,3 @@
-# FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
-
 # imports
 import os  # os is used to get environment variables IP & PORT
 from flask import Flask  # Flask is the web app that we will customize
@@ -25,17 +23,21 @@ app = Flask(__name__)  # create an app
 @app.route('/')
 @app.route('/index')
 def index():
-    a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
-    return render_template("index.html", user=a_user)
+    # a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
+    #  check if a user is saved in session
+    if session.get('user'):
+        return render_template("index.html", user=session['user'])
+    return render_template("index.html")
 
 
 @app.route('/notes')
 def get_notes():
     # retrieve user from database
     # a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
+    #  check if a user is saved in session
     if session.get('user'):
         # retrieve notes from database
-        my_notes = db.session.query(Note).all()
+        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
 
         return render_template('notes.html', notes=my_notes, user=session['user'])
     else:
@@ -44,48 +46,79 @@ def get_notes():
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
-    # retrieve user from database
-    a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
-    # retrieve note from database
-    my_notes = db.session.query(Note).filter_by(id=note_id).one()
+    #  check if a user saved in session
+    if session.get('user'):
+        # retrieve user from database
+        # a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
+        # retrieve note from database
+        my_notes = db.session.query(Note).filter_by(id=session['user_id']).one()
 
-    return render_template('note.html', note=my_notes)
-    # return "Welcome, Notes App User!"
+        return render_template('note.html', note=my_notes, user=session['user'])
+        # return "Welcome, Notes App User!"
+    else:
+        # user is not in session redirect to login
+        return redirect(url_for('login'))
 
 
 @app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
 def update_note(note_id):
-    # check method used for request
-    if request.method == 'POST':
-        #  get title data
-        title = request.form['title']
-        #  get note data
-        text = request.form['noteText']
-        note = db.session.query(Note).filter_by(id=note_id).one()
-        #  update note data
-        note.title = title
-        note.text = text
-        #  update note in DB
-        db.session.add(note)
+    #  check if a user saved in session
+    if session.get('user'):
+        # check method used for request
+        if request.method == 'POST':
+            #  get title data
+            title = request.form['title']
+            #  get note data
+            text = request.form['noteText']
+            note = db.session.query(Note).filter_by(id=note_id).one()
+            #  update note data
+            note.title = title
+            note.text = text
+            #  update note in DB
+            db.session.add(note)
+            db.session.commit()
+
+            return redirect(url_for('get_notes'))
+        else:
+            # GET request - show new note form to edit note
+            # retrieve user from database
+            # a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
+            # retrieve note from database
+            my_note = db.session.query(Note).filter_by(id=note_id).one()
+
+            return render_template('new.html', note=my_note, user=session['user'])
+    else:
+        # user is not in session redirect to login
+        return redirect(url_for('login'))
+
+
+# @app.route('/notes/delete/<note_id>', methods=['POST'])
+# def delete_note(note_id):
+#     #  check if a user is saved in session
+#     if session.get('user'):
+#         # retrieve note from database
+#         my_note = db.session.query(Note).filter_by(id=note_id).one()
+#         db.session.delete(my_note)
+#         db.session.commit()
+#
+#         return redirect(url_for('get_notes'))
+#     else:
+#         # user is not in session redirect to login
+#         return redirect(url_for('login'))
+
+@app.route('/notes/delete/<note_id>', methods=['POST'])
+def delete_note(note_id):
+    # check if a user is save in session
+    if session.get('user'):
+        # retrieve note from database
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+        db.session.delete(my_note)
         db.session.commit()
 
         return redirect(url_for('get_notes'))
     else:
-        # GET request - show new note form to edit note
-        # retrieve user from database
-        a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
-        # retrieve note from database
-        my_note = db.session.query(Note).filter_by(id=note_id).one()
-
-        return render_template('new.html', note=my_note, user=a_user)
-
-
-@app.route('/notes/delete/<note_id>', methods=['POST'])
-def delete_note(note_id):
-    # retrieve note from database
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    db.session.delete(my_note)
-    db.session.commit()
+        # user is not in session redirect to login
+        return redirect(url_for('login'))
 
 
 @app.route('/register', methods=['POST', 'GET'])
@@ -141,32 +174,37 @@ def login():
 @app.route('/notes/new', methods=['GET', 'POST'])
 def new_note():
     # a_user = {'name': 'Mohammad', 'email': 'mogli@uncc.edu'}
-    a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
-    # check method used for request
-    print('request method is', request.method)
-    if request.method == 'POST':
-        # get title data
-        title = request.form['title']
-        # get note data
-        text = request.form['noteText']
-        # create date stamp
-        from datetime import date
-        today = date.today()
-        # format date mm/dd/yyy
-        today = today.strftime("%m-%d-%y")
+    # a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
+    #  check if a user is saved in session
+    if session.get('user'):
+        # check method used for request
+        # print('request method is', request.method)
+        if request.method == 'POST':
+            # get title data
+            title = request.form['title']
+            # get note data
+            text = request.form['noteText']
+            # create date stamp
+            from datetime import date
+            today = date.today()
+            # format date mm/dd/yyy
+            today = today.strftime("%m-%d-%y")
 
-        new_record = Note(title, text, today)
-        db.session.add(new_record)
-        db.session.commit()
+            new_record = Note(title, text, today, session['user_id'])
+            db.session.add(new_record)
+            db.session.commit()
 
-        return redirect(url_for('get_notes'))
-        # request_data = request.form
-        # return f"data: {request_data} !"
+            return redirect(url_for('get_notes'))
+            # request_data = request.form
+            # return f"data: {request_data} !"
+        else:
+            # retrieve user from database
+            # a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
+            # GET request - show new note form
+            return render_template('new.html', user=session['user'])
     else:
-        # GET request - show new note form
-        # retrieve user from database
-        a_user = db.session.query(User).filter_by(email='mnaeem1@uncc.edu').one()
-        return render_template('new.html', user=a_user)
+        # user is not in session redirect to login
+        return redirect(url_for('login'))
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
